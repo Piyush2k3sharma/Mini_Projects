a189bf7b5450166cd1a364f939bd864865123778
Piyush Sharma <piyush2k3sharma@gmail.com>
2024-10-09 15:30:26 +0530

    Updated Code

--- Jokes_app.py
+++ Jokes_app.py
@@ -4,28 +4,35 @@ window = ctk.CTk()
 window.title('  JOKES')
 window.geometry('800x600')
 
+'''Frame 1'''
+
 frame_1 = ctk.CTkFrame(master=window,fg_color='red',corner_radius=0,height=600,width=200)
 frame_1.place(x=0,y=0)
 
-logo = ctk.CTkLabel(frame_1,height=150,width=200)
+logo = ctk.CTkLabel(frame_1,height=150,width=200,fg_color="Blue",corner_radius=20)
 logo.place(x=0,y=0)
 
-category = ctk.CTkOptionMenu(frame_1,values=["Any","Custom"])
-category.place(x=0,y=200)
+category = ctk.CTkOptionMenu(frame_1,values=["Any","Custom"],height=30,width=180,corner_radius=15)
+category.place(x=10,y=155)
 
 # radiobuttongroup here for custom types
 
-amount_input = ctk.CTkEntry(frame_1, placeholder_text="Enter Amount here : ")
-amount_input.place(x=0,y=100)
+amount_label = ctk.CTkLabel(frame_1,height=20,width=180,corner_radius=20,text="Enter The Amount Of Jokes")
+amount_label.place(x=10,y=200)
 
-btn = ctk.CTkButton(frame_1,text="Generate")
-btn.place(x=0,y=150)
+amount_input = ctk.CTkEntry(frame_1, placeholder_text="Enter Amount here : ",height=30,width=180,corner_radius=4)
+amount_input.place(x=10,y=220)
+
+btn = ctk.CTkButton(frame_1,text="Generate",height=40,width=180,corner_radius=5)
+btn.place(x=10,y=270)
+
+'''Frame 2'''
 
 frame_2 = ctk.CTkFrame(master=window,fg_color='blue',corner_radius=0,height=600,width=600)
 frame_2.place(x=200,y=0)
 
-label1 = ctk.CTkLabel(frame_2,text="Here is your joke")
-label1.place(x=0,y=0)
+joke_label = ctk.CTkLabel(frame_2,text="Here is your joke",width=500,height=100)
+joke_label.place(x=50,y=50)
 
 text = '''
 Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:
@@ -35,11 +42,18 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor i
 The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.) that doesn't distract from the layout. A practice not without controversy, laying out pages with meaningless filler text can be very useful when the focus is meant to be on design, not content.
 
 The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it's seen all around the web; on templates, websites, and stock designs. Use our generator to get your own, or read on for the authoritative history of lorem ipsum.
+Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+
+The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.) that doesn't distract from the layout. A practice not without controversy, laying out pages with meaningless filler text can be very useful when the focus is meant to be on design, not content.
+
+The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it's seen all around the web; on templates, websites, and stock designs. Use our generator to get your own, or read on for the authoritative history of lorem ipsum.
 '''
 
-output_text = ctk.CTkTextbox(frame_2)
+output_text = ctk.CTkTextbox(frame_2,width=530,height=400)
 output_text.insert('0.0',text)
 output_text.configure(state="disabled")
-output_text.place(x=100,y=100)
+output_text.place(x=35,y=150)
 
 window.mainloop()
