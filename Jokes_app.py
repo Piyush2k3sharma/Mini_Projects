8af9b7541a10e86dcb3f5d7fcdfe7c049d36b51a
Piyush Sharma <piyush2k3sharma@gmail.com>
2024-10-17 16:21:16 +0530

    Functionality updated & Logo added:

--- Jokes_app.py
+++ Jokes_app.py
@@ -1,8 +1,11 @@
 import customtkinter as ctk
+from PIL import Image
 
 window = ctk.CTk()
 window.title('  JOKES')
 window.geometry('800x600')
+
+
 
 ''' Color scheme '''
 
@@ -17,6 +20,8 @@ L_PRIMARY_COLOR = ""
 L_SECONDARY_COLOR = ""
 L_TERTIARY_COLOR = ""
 L_TEXT_COLOR = ""
+
+
 
 def update_radio_buttons(category):
     if category == "Custom":
@@ -34,47 +39,53 @@ def update_radio_buttons(category):
         types_btn5.configure(state="disabled")
         types_btn6.configure(state="disabled")
 
+
 def disp():
-    category = category.get()
+    if category.get()=='Any':
+        c = 'Any'
+    else:
+        c = 'Custom'
     amount = amount_input.get()
-    joke_type = joke_type
-    print(category)
-    print(amount)
-    print(joke_type)
+    j_type = joke_type
+    print(f"category : {c},category value : {categ_val},amount : {amount},joke type : {j_type}")
+
 
 '''Frame 1'''
 
 frame_1 = ctk.CTkFrame(master=window,fg_color=D_PRIMARY_COLOR,corner_radius=0,height=600,width=200)
 frame_1.place(x=0,y=0)
 
-logo = ctk.CTkLabel(frame_1,height=150,width=200,fg_color=D_TERTIARY_COLOR,corner_radius=20)
+my_image = ctk.CTkImage(light_image=Image.open(r"C:\Users\ayush\OneDrive\Desktop\Netmaxx\Python files\Mini projects\images\10254401.png"),dark_image=Image.open(r"C:\Users\ayush\OneDrive\Desktop\Netmaxx\Python files\Mini projects\images\10254401.png"),size=(165, 165))
+
+logo = ctk.CTkLabel(frame_1,height=150,width=200,fg_color=D_PRIMARY_COLOR,image=my_image,text="",corner_radius=20)
 logo.place(x=0,y=0)
 
 category = ctk.CTkOptionMenu(frame_1,values=["Any","Custom"],fg_color=D_TERTIARY_COLOR,height=30,width=180,corner_radius=15,command=lambda event: update_radio_buttons(category.get()))
 category.place(x=10,y=155)
 
 # radiobuttongroup here for custom types
-categ_val = ''
+categ_val = None
 def on_select(value):
+    global categ_val
     categ_val = value
 
 var1 = ctk.StringVar()
-types_btn1 = ctk.CTkRadioButton(frame_1,text="Programming",variable=var1,command=lambda: on_select(var1.get()))
+types_btn1 = ctk.CTkRadioButton(frame_1,text="Programming",variable=var1,value=0,command=lambda: on_select(var1.get()))
 types_btn1.place(x=10,y=195)
 
-types_btn2 = ctk.CTkRadioButton(frame_1,text="Misc",variable=var1,command=lambda: on_select(var1.get()))
+types_btn2 = ctk.CTkRadioButton(frame_1,text="Misc",variable=var1,value=1,command=lambda: on_select(var1.get()))
 types_btn2.place(x=10,y=220)
 
-types_btn3 = ctk.CTkRadioButton(frame_1,text="Dark",variable=var1,command=lambda: on_select(var1.get()))
+types_btn3 = ctk.CTkRadioButton(frame_1,text="Dark",variable=var1,value=2,command=lambda: on_select(var1.get()))
 types_btn3.place(x=10,y=245)
 
-types_btn4 = ctk.CTkRadioButton(frame_1,text="Pun",variable=var1,command=lambda: on_select(var1.get()))
+types_btn4 = ctk.CTkRadioButton(frame_1,text="Pun",variable=var1,value=3,command=lambda: on_select(var1.get()))
 types_btn4.place(x=10,y=270)
 
-types_btn5 = ctk.CTkRadioButton(frame_1,text="Spooky",variable=var1,command=lambda: on_select(var1.get()))
+types_btn5 = ctk.CTkRadioButton(frame_1,text="Spooky",variable=var1,value=4,command=lambda: on_select(var1.get()))
 types_btn5.place(x=10,y=295)
 
-types_btn6 = ctk.CTkRadioButton(frame_1,text="Christmas",variable=var1,command=lambda: on_select(var1.get()))
+types_btn6 = ctk.CTkRadioButton(frame_1,text="Christmas",variable=var1,value=5,command=lambda: on_select(var1.get()))
 types_btn6.place(x=10,y=320) 
 
 update_radio_buttons("Any")
@@ -87,7 +98,7 @@ amount_label.place(x=10,y=350)
 amount_input = ctk.CTkEntry(frame_1, placeholder_text="Enter Amount here : ",fg_color=D_TERTIARY_COLOR,height=30,width=180,corner_radius=4)
 amount_input.place(x=10,y=370)
 
-joke_type = ''
+joke_type = None
 def on_click(value):
     global joke_type
     joke_type = value
@@ -99,10 +110,10 @@ type_label = ctk.CTkLabel(frame_1,text="Select The Type of Jokes",
     height=20,width=180,corner_radius=20)
 type_label.place(x=10,y=405)
 
-joke_type_btn1 = ctk.CTkRadioButton(frame_1,text="Single",variable=var2,command=lambda: on_click(var2.get()))
+joke_type_btn1 = ctk.CTkRadioButton(frame_1,text="Single",variable=var2,value=0,command=lambda: on_click(var2.get()))
 joke_type_btn1.place(x=10,y=430)
 
-joke_type_btn2 = ctk.CTkRadioButton(frame_1,text="Two Part",variable=var2,command=lambda: on_click(var2.get()))
+joke_type_btn2 = ctk.CTkRadioButton(frame_1,text="Two Part",variable=var2,value=1,command=lambda: on_click(var2.get()))
 joke_type_btn2.place(x=10,y=455)
 
 generate_btn = ctk.CTkButton(frame_1,text="Generate",fg_color=D_TERTIARY_COLOR,height=40,width=180,corner_radius=5,command=disp)
@@ -144,5 +155,6 @@ window.mainloop()
 '''
 
 Color pallete website - https://paletton.com/
+Image logo link - https://www.freepik.com/free-psd/3d-emoji-isolated_133742319.htm#fromView=search&page=1&position=18&uuid=3f2b9d28-0797-454e-8cfe-761b7e381b24
 
 '''
